01902e02e06b54c739f646a4e5a9171394db3ec7
Frederic MAMATH <fredericmamath@gmail.com>
2023-01-22 13:10:37 +0100

    :construction: Adds arg parser

--- main.py
+++ main.py
@@ -2,25 +2,32 @@
 
 import json
 import requests
+import argparse
 from datetime import datetime
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--alert_id", help="Alert ID to scrap")
+parser.add_argument("--authentication_token", help="Authentication token found chrome/Application/Cookies/LA_API_TOKEN")
+args = parser.parse_args()
+
+alert_id=args.alert_id
+authentication_token=args.authentication_token
 
 now = datetime.now()
 timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
 
-url = "https://api.jinka.fr/apiv2/alert/2306da5e990d75bb2e00a0ea2381f63c/dashboard?filter=all&page=1&rrkey=xdwna9&sorting="
+url = f"https://api.jinka.fr/apiv2/alert/{alert_id}/dashboard?filter=all&page=1&rrkey=xdwna9&sorting="
 
 headers = {
     "accept": "*/*",
     "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
     "authorization":
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OTMzMzgxLCJlbWFpbCI6ImFudG9ueS52ZW9wYXNldXRoQGVwZmVkdS5mciIsImNyZWF0ZWRfYXQiOiIyMDIzLTAxLTIyVDA5OjMzOjUzLjAwMFoiLCJpYXQiOjE2NzQzODQ1NjIsImV4cCI6MTY3Njk3NjU2Mn0.iAcn9knYNqxVPsM-lxUVqThS5rLzIjDwnohNVrn5OeI",
+        f"Bearer {authentication_token}",
 
 }
-# Trigger curl request
 response = requests.get(url, headers=headers)
 
 data = response.json()
 
-# Write JSON data to file
-with open(f"jinka-{timestamp}.json", "a") as f:
+with open(f"data/jinka-{timestamp}.json", "a") as f:
     json.dump(data, f)
