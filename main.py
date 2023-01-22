c3d443fe5e55a248db70756f6a77f6e9de94899f
Frederic MAMATH <fredericmamath@gmail.com>
2023-01-22 13:56:00 +0100

    :memo: Updates python script structure

--- main.py
+++ main.py
@@ -5,17 +5,6 @@ import requests
 import argparse
 from datetime import datetime
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--alert_id", help="Alert ID to scrap")
-parser.add_argument("--authentication_token", help="Authentication token found chrome/Application/Cookies/LA_API_TOKEN")
-args = parser.parse_args()
-
-alert_id=args.alert_id
-authentication_token=args.authentication_token
-
-now = datetime.now()
-timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
-
 def get_alert_ads_from_page(page_index):
     url = f"https://api.jinka.fr/apiv2/alert/{alert_id}/dashboard?filter=all&page={page_index}&rrkey=xdwna9&sorting="
 
@@ -36,6 +25,17 @@ def get_last_page_index():
     return first_page_response['pagination']['nbPages']
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--alert_id", help="Alert ID to scrap", required=True)
+    parser.add_argument("--authentication_token", help="Authentication token found in Chrome > Application > Cookies > LA_API_TOKEN", required=True)
+    args = parser.parse_args()
+
+    alert_id=args.alert_id
+    authentication_token=args.authentication_token
+
+    now = datetime.now()
+    timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+
     print(f"Scraping Jinka for the alert {alert_id}...")
     last_page_index = get_last_page_index()
     print(f"{last_page_index} pages found !")
@@ -54,4 +54,5 @@ def main():
     with open(output_file_name, "a") as f:
         f.write(pretty_json)
 
-main()
+if __name__ == "__main__":
+    main()
